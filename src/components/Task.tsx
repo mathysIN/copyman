8a230a576e9d05d99e2629936fa1a0650416188f
mathys <mr.red111111@gmail.com>
2024-04-27 20:05:37 +0200

    Added base code for style content links

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,10 +2,27 @@
 
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
-import { NoteType } from "~/server/db/redis";
+import { link } from "fs";
+import { useEffect, useState } from "react";
+import {
+  LinkMetadata,
+  extractLinksFromString,
+  getLinkMetadata,
+  getLinkMetadataFromClient,
+} from "~/lib/utils";
+import type { NoteType } from "~/server/db/redis";
+import type urlMetadata from "url-metadata";
+import Image from "next/image";
 
 const REQUEST_DELAY = 800;
+
+type LinksWithMeta = {
+  link: string;
+  metadata: {
+    image: string;
+    title: string;
+  };
+};
 
 export function Task({
   content,
@@ -16,8 +33,12 @@ export function Task({
 }) {
   const [value, setValue] = useState(content.content ?? "");
   const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null);
+  const [deleting, setDeleting] = useState(false);
+  const [modifying, setModifying] = useState(false);
+  const [linksWithMeta, setLinksWithMetaData] = useState<LinksWithMeta[]>([]);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setModifying(true);
     const newValue = e.target.value;
     setValue(newValue);
     if (timerId) {
@@ -28,6 +49,7 @@ export function Task({
   };
 
   const editTask = (newValue: string) => {
+    setModifying(false);
     fetch("/api/notes", {
       method: "PATCH",
       body: JSON.stringify({ content: newValue, taskId: content.id }),
@@ -36,29 +58,77 @@ export function Task({
     });
   };
 
+  const links = extractLinksFromString(value);
+
+  useEffect(() => {
+    (async () => {
+      const _linksWithMeta: LinksWithMeta[] = [];
+      for (const link of links) {
+        const metadata = await getLinkMetadataFromClient(link);
+        if (
+          metadata &&
+          ((metadata["image"] && metadata["url"]) ||
+            (metadata["favicons"] && metadata["favicons"][0])) &&
+          metadata["title"]
+        )
+          _linksWithMeta.push({
+            link: link,
+            metadata: {
+              title: metadata["title"],
+              image: metadata["image"]
+                ? metadata["url"] + metadata["image"]
+                : metadata["favicons"][0]["href"],
+            },
+          });
+      }
+      setLinksWithMetaData(_linksWithMeta);
+    })();
+  }, [value]);
+
   return (
     <div
       key={content.id}
-      className="flex flex-row gap-2 rounded-md border-2 border-gray-300 bg-white px-2 py-2 text-black"
+      className={`${deleting && "animate-pulse cursor-wait opacity-75"} flex flex-col gap-2 rounded-md border-2 border-gray-300 bg-white px-2 py-2 text-black`}
     >
-      <textarea
-        value={value}
-        onChange={handleChange}
-        className="h-fit flex-grow"
-      ></textarea>
-      <button
-        onClick={() => {
-          fetch("/api/notes", {
-            method: "DELETE",
-            body: JSON.stringify({ taskId: content.id }),
-          }).then(() => {
-            onDeleteTask(content.id);
-          });
-        }}
-        className="min-w-min text-red-400"
-      >
-        <FontAwesomeIcon icon={faTrash} />
-      </button>
+      <div className="flex flex-row gap-2">
+        <textarea
+          disabled={deleting}
+          value={value}
+          onChange={handleChange}
+          className={`${deleting && "cursor-wait"} h-fit flex-grow`}
+        ></textarea>
+        <button
+          disabled={deleting}
+          className={`${deleting && "cursor-wait"} min-w-min text-red-400`}
+          onClick={async () => {
+            setDeleting(true);
+            fetch("/api/notes", {
+              method: "DELETE",
+              body: JSON.stringify({ taskId: content.id }),
+            })
+              .then(() => {
+                onDeleteTask(content.id);
+              })
+              .catch(() => {
+                setDeleting(false);
+              });
+          }}
+        >
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+      </div>
+      <div className="flex flex-row flex-wrap">
+        {linksWithMeta.map((link) => (
+          <a
+            href={link.link}
+            target="_blank"
+            className="flex flex-row items-center gap-2 rounded-lg border-2 bg-neutral-800 px-2 py-1 text-white"
+          >
+            <p>{link.metadata.title}</p>
+            <img src={link.metadata.image} width={20} height={20} alt="" />
+          </a>
+        ))}
+      </div>
     </div>
   );
 }

ad2e3bdedf5cd02ee44250562cb3c1a420ab99e1
mathys <mr.red111111@gmail.com>
2025-01-20 20:52:39 +0100

    added manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Copyman",
+    short_name: "Copyman",
+    description: "Un presse papier pas sécurisé mais pratique pour mon travail",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#287d7c",
+    theme_color: "#287d7c",
+    icons: [
+      {
+        src: "/favicon.png",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}

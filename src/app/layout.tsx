ea8ccb56c9e51489273dc8badf74b79508919fc8
mgouzenes <mgouzenes@pactenovation.fr>
2025-10-27 10:58:45 +0100

    fixed weird icon size at launch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import { Inter } from "next/font/google";
 import { Toaster } from "~/components/ui/toaster";
 import ServiceWorkerRegister from "~/components/ServiceWorkerRegister";
 import React from "react";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 
 const inter = Inter({
   subsets: ["latin"],
